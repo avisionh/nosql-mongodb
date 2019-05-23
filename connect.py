db3f781974d0ea060d5f8deb3c152d89751d3a7c
AH <a_vision@hotmail.co.uk>
2019-05-23 17:56:28 +0100

    feat: download countries data.
    This is to provider a richer set of data to perform MongoDB operations on.

--- connect.py
+++ connect.py
@@ -27,7 +27,7 @@ kaggle.api.authenticate()
 class Connect(object):
     @staticmethod    
     def get_connection():
-        return MongoClient("mongodb://user_readwrite:<password>@cluster-open-shard-00-00-kzzlc.mongodb.net:27017,cluster-open-shard-00-01-kzzlc.mongodb.net:27017,cluster-open-shard-00-02-kzzlc.mongodb.net:27017/test?replicaSet=cluster-open-shard-0&authSource=admin&ssl=true")
+        return MongoClient("mongodb://user_readwrite:<pw>@cluster-open-shard-00-00-kzzlc.mongodb.net:27017,cluster-open-shard-00-01-kzzlc.mongodb.net:27017,cluster-open-shard-00-02-kzzlc.mongodb.net:27017/test?replicaSet=cluster-open-shard-0&authSource=admin&ssl=true")
 
 # 2. Call the class just created.
 client = Connect.get_connection()
@@ -46,12 +46,17 @@ for authors in cursor:
 path_folder = "C:/Users/a_vis/Documents/Data Science/Python/MongoDB/data"
 kaggle.api.dataset_download_files('adithyarganesh/english-premier-league-player-data-20182019', 
                                   path = path_folder, 
-                                  unzip=True)
+                                  unzip = True)
 
 # 2. Import EPL data into Python
 with open(path_folder + '/fpl_data_2018_2019.json') as data_football:
     file_data = json.load(data_football)
 
+# convert to list for importing as multiple documents
+# this does't quite work since we don't have key-value pairs
+# for players e.g. player:"Rolando Aarons"
+# file_data_list = file_data.items()
+    
 # 3. Create database and collection for new data
 db = client.football
 collection_england = db.england
@@ -62,3 +67,33 @@ collection_england.insert_one(file_data)
 cursor = db.england.find({})
 for england in cursor:
      pprint(england)
+     
+# 5. Drop collection in case import went wrong
+db.england.drop()
+
+# - Data Import: Secondary - #
+# 1. Import Country data for constructing links between country data
+kaggle.api.dataset_download_files('timoboz/country-data',
+                                  path = path_folder,
+                                  unzip = True)
+
+# 2. Import country json file into Python session
+data_dicts = []
+for file in os.listdir(path_folder):
+    full_filename = "%s/%s" % (path_folder, file)
+    with open(full_filename,'r') as fi:
+        dict = json.load(fi)
+        data_dicts.append(dict)
+        
+del dict; del file; del full_filename
+
+# 3. Remove 4th and 5th list items since they are already in database
+# note: use pop() instead of del() because want to return the dict item
+#        being removed and store in a list
+# note: perform same operation 2 times so want to do a loop
+#        but don't want to store iterator so use '_' instead
+# reference: https://stackoverflow.com/a/2970808
+data_remove = [] 
+for _ in range(2):
+    data_remove.append(data_dicts.pop(3))
+
